314bb2da282eb0dffd2e9a862eeafdd2440ef35e
oAzv <945364073@qq.com>
2018-10-31 16:08:45 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import pandas as pd
+import numpy as np
+import time
+
+if __name__ == '__main__':
+    root_dir = os.getcwd().replace('\\', '/') + '/'
+    print(root_dir)
